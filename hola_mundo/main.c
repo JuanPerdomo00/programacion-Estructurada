e4f0579830bf9ad10c0cdc9bd02d951db8aa1964
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-06 10:01:59 -0500

    varibles y holamundo

---
+++ hola_mundo/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    printf("\n Hola Mundo en c \n\n");
+    int numero_favA = 9;
+    float numero_PI = 3.1415;
+    double numeroA = 1.2;
+    char letra_fav = 'H';
+
+    printf("Mi numero favorito es: %i \n\n", numero_favA);
+
+    printf("El valor de numero PI es: %f \n\n", numero_PI);
+
+    printf("Mi numero de double es: %f \n\n", numeroA);
+
+    printf("Mi letra fav es: %c \n\n", letra_fav);
+    return 0;
+}
