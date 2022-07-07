6c3b40faf9020ef5808893a062b7939e84f56daf
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-07 15:54:41 -0500

    operadores aritmeticos

---
+++ operadore_aritmeticos/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int suma, modulo;
+    float res, div, mult;
+
+    suma = 2 + 10;
+    res = 3.2 - 2.1;
+    mult = 7 * 3;
+    div = 6.3 / 3.0;
+    modulo = 3 % 2;
+
+    printf("suma: %i\n, resta: %f\n, multiplicacion: %f\n, divicion: %f\n, modulo: %i\n", suma,res,mult,div, modulo);
+
+
+    return 0;
+}
